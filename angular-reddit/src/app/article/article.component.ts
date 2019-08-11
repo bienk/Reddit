7426f12eea8acd1be81c55ea242097bcf3962aa5
EC2 Default User <ec2-user@ip-172-31-42-133.ec2.internal>
2019-08-11 15:52:11 +0000

    6.1

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostBinding} from '@angular/core';
-
+import { Article } from './article.model';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -11,18 +11,19 @@ export class ArticleComponent implements OnInit {
   votes: number;
   title: string;
   link: string;
-
+  article : Article;
   constructor() {
     this.title = 'Angular 2';
     this.link = 'http://angular.io';
     this.votes = 10;
+    this.article =  new Article('Angular 2','http://angular.io',10);
   }
 voteUp():Boolean { //Aggiunto tipo di ritorno
-    this.votes += 1;
+    this.article.voteUp();
     return false; //Non propagare l'evento 
 }
 voteDown():Boolean{
-    this.votes -= 1;
+    this.article.voteDown();
     return false; //Non propagare l'evento 
 }
 
